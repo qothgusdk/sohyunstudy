6e607ffa036eec11e0ae68051fc0a9537ec3ade9
Baesohyun <qothgusdk@naver.com>
2023-04-20 14:17:29 +0900

    sohyun study

--- src/study.java
+++ src/study.java
@@ -1,17 +1,19 @@
  class Calculator {
+        static double pi = 3.14;
+        static int base = 0;
         int left, right;
 
-        public void setOprands(int left, int right) {
-            this.left = left;
-            this.right = right;
+        public void setOprands(int l, int r) {
+            left = l;
+            right = r;
         }
 
         public void sum() {
-            System.out.println(this.left + this.right);
+            System.out.println(left + right + base);
         }
 
         public void avg() {
-            System.out.println((this.left + this.right) / 2);
+            System.out.println((left + right) / 2);
         }
     }
 
@@ -20,11 +22,14 @@
             Calculator c1 = new Calculator();
             c1.setOprands(10, 20);
             c1.sum();
-            c1.avg();
 
             Calculator c2 = new Calculator();
             c2.setOprands(20, 40);
             c2.sum();
-            c2.avg();
+
+            Calculator.base = 10; //base 0->10지정
+            c1.sum();
+            c2.sum();
+            System.out.println(Calculator.pi);
         }
 }
