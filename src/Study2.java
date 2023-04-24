8185a00d8098073148d45306dac138893223764c
Baesohyun <qothgusdk@naver.com>
2023-04-24 13:37:33 +0900

    sohyun study2

--- src/Study2.java
+++ src/Study2.java
@@ -1,11 +1,27 @@
-public class Study2 {
-    static void a(){
-        int i = 0;
+class Calculator {
+    int left, right;
+    public void serOarands(int l,int r){
+        left = l;
+        right = r;
     }
+    public void sum(){
+        System.out.println(left+right);
+    }
+    public void  avg(){
+        System.out.println((left+right)/2);
+    }
+}
+class SubstractionableCalculator extends Calculator{
+    public void substract(){
+        System.out.println(left-right);
+    }
+}
+public class Study2 {
     public static void main(String[] args) {
-       for (int i = 0; i<5; i++){
-           a();
-           System.out.println(i);
-       }
+        SubstractionableCalculator a = new SubstractionableCalculator();
+        a.serOarands(10, 20);
+        a.sum();
+        a.avg();
+        a.substract();
     }
 }
