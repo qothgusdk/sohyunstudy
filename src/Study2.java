e2e45614c3392fb7a26a86a55b874b1a78f9ce95
Baesohyun <qothgusdk@naver.com>
2023-04-28 14:57:12 +0900

    sohyun study2

--- src/Study2.java
+++ src/Study2.java
@@ -1,24 +1,40 @@
-class Calculator{
-    private int left, right;
+abstract class Calculator{
+    int left, right;
     public void setOprands (int l, int r){
         left = l;
         right = r;
     }
-    private int sum(){
-        return left+right;
+    public abstract void sum ();
+    public abstract void  avg ();
+    public void run () {
+        sum();
+        avg();
     }
-    public void sumDecoPlus () {
-        System.out.println("++++"+sum()+"++++");
+}
+class CalculatorDecoPlus extends Calculator {
+    public void sum(){
+        System.out.println(left+right);
     }
-    public void sumDecoMinus () {
-        System.out.println("----"+sum()+"----");
+    public void avg(){
+        System.out.println((left+right)/2);
+    }
+}
+class CalculatorDecoMinus extends Calculator {
+    public void sum(){
+        System.out.println("- sum :"+(left+right) );
+    }
+    public void avg(){
+        System.out.println("- avg :"+((left+right)/2));
     }
 }
 public class Study2  {
     public static void main(String[] args) {
-        Calculator x = new Calculator();
-        x.setOprands(10, 20);
-        x.sumDecoPlus();
-        x.sumDecoMinus();
+        CalculatorDecoPlus a = new CalculatorDecoPlus();
+        a.setOprands(10, 20);
+        a.run();
+
+        CalculatorDecoMinus b = new CalculatorDecoMinus();
+        b.setOprands(10, 20);
+        b.run();
     }
 }
